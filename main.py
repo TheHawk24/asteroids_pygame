a38822f73e4e63deaa7841721be310732cfbd53e
TheHawk <patricklehloenya2@gmail.com>
2024-12-16 11:29:39 +0200

    added a player object(triangle)

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import pygame
 from constants import *
+import player
 
 
 def main():
     print("Starting asteroids!")
 
-    numpass, numfail = pygame.init()
+    pygame.init()
 
     if not pygame.get_init():
         return
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    time = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    triangle_player = player.Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        triangle_player.draw(screen)
         pygame.display.flip()
+        time_passed = time.tick(60)
+        dt = time_passed/1000
 
 
 if __name__ == "__main__":
